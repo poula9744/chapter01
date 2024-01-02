b61a6d28b81a58d80e536efcbee7bfde98cd480f
poula9744 <poula9744@naver.com>
2024-01-02 12:45:06 +0900

    cmd

--- src/com/javaex/ex05/Ex10.java
+++ src/com/javaex/ex05/Ex10.java
@@ -9,6 +9,18 @@ public class Ex10 {
 			System.out.println(i + " 번째 값: " + args[i]);
 		}
 
+		//-h
+		if(args[0].equals("-h")) {
+			System.out.println("[-h]: 도움말");
+			System.out.println("[-v]: 버전정보");
+			System.out.println("[-m]: 만든 사람");
+		} else if(args[0].equals("-v")) {
+			System.out.println("버전 1.7");
+		} else if(args[0].equals("-m")) {
+			System.out.println("만든사람: aaa");
+		} else {
+			System.out.println("옵션을 확인하세요.");
+		}
 	}
 
 }
